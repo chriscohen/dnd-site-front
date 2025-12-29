935c0c3ac7b1cffafbd93a001952c21b63b0da91
Chris Cohen <malone82@gmail.com>
2025-12-29 17:28:15 +0000

    Refactored DndHeading component to simplify template structure and improve class handling logic.

--- app/components/headings/DndHeading.vue
+++ app/components/headings/DndHeading.vue
@@ -7,71 +7,33 @@ const props = defineProps<{
     underline?: boolean
 }>();
 
-const divClasses = props.underline ? 'border-b' : '';
+if (props.underline) { console.log('underline is true')} else { console.log('underline is false')}
 
-const divVariableClasses = [
+const divClasses = [
+    props.underline && 'border-b',
     props.class,
-    props.underlineColor ? 'border-' + props.underlineColor : 'border-gray-400'
+    props.underline ? (props.underlineColor ? 'border-' + props.underlineColor : 'border-gray-400') : null
 ].join(' ');
+
 const staticClasses = 'font-eaves';
 const variableClasses = [
     staticClasses
 ].join(' ');
-const headingSize = props.size ?? '1';
 </script>
 
 <template>
-    <div v-if="headingSize === '1'" :class="divVariableClasses + ' ' + divClasses">
-        <h1 :class="variableClasses">
-            <template v-if="loading">
-                <USkeleton class="h-4 w-full"/>
-            </template>
-            <template v-else><slot/></template>
-        </h1>
+    <header>
+        <div class="flex flex-row gap-4 justify-between items-center" :class="divClasses">
+            <component :is="'h' + size" :class="variableClasses">
+                <template v-if="loading">
+                    <USkeleton class="h-4 w-full"/>
+                </template>
+                <template v-else><slot/></template>
+            </component>
+
+            <slot name="controls"/>
+        </div>
+
         <slot name="subtitle"/>
-    </div>
-    <div v-else-if="headingSize === '2'" :class="divVariableClasses + ' ' + divClasses">
-        <h2 :class="variableClasses" class="text-4xl">
-            <template v-if="loading">
-                <USkeleton class="h-4 w-full"/>
-            </template>
-            <template v-else><slot/></template>
-        </h2>
-        <slot name="subtitle"/>
-    </div>
-    <div v-else-if="headingSize === '3'" :class="divVariableClasses + ' ' + divClasses">
-        <h3 :class="variableClasses" class="text-3xl">
-            <template v-if="loading">
-                <USkeleton class="h-4 w-full"/>
-            </template>
-            <template v-else><slot/></template>
-        </h3>
-        <slot name="subtitle"/>
-    </div>
-    <div v-else-if="headingSize === '4'" :class="divVariableClasses + ' ' + divClasses">
-        <h4 :class="variableClasses" class="text-2xl">
-            <template v-if="loading">
-                <USkeleton class="h-4 w-full"/>
-            </template>
-            <template v-else><slot/></template>
-        </h4>
-        <slot name="subtitle"/>
-    </div>
-    <div v-else-if="headingSize === '5'" :class="divVariableClasses + ' ' + divClasses">
-        <h5 :class="variableClasses" class="text-xl">
-            <template v-if="loading">
-                <USkeleton class="h-4 w-full"/>
-            </template>
-            <template v-else><slot/></template>
-        </h5>
-        <slot name="subtitle"/>
-    </div>
-    <div v-else-if="headingSize === '6'" :class="divVariableClasses + ' ' + divClasses">
-        <h6 :class="variableClasses" class="text-lg">
-            <template v-if="loading">
-                <USkeleton class="h-4 w-full"/>
-            </template>
-            <template v-else><slot/></template>
-        </h6>
-    </div>
+    </header>
 </template>
