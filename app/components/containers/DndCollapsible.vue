e24cd28cd835619a80dbd1a5ac5622e00f37c609
Chris Cohen <malone82@gmail.com>
2025-12-29 17:27:44 +0000

    Refactored DndCollapsible to use DndHeading component and simplified template structure.

--- app/components/containers/DndCollapsible.vue
+++ app/components/containers/DndCollapsible.vue
@@ -1,30 +1,28 @@
 ﻿<script setup lang="ts">
+import DndHeading from "~/components/headings/DndHeading.vue";
+
 const props = defineProps<{
     class?: string
     startOpen?: boolean
     title: string
 }>();
 
-const open: Ref<boolean> = ref(props.startOpen ?? false);
+
 </script>
 
 <template>
     <UCollapsible
-        v-model:open="open"
-        class="rounded-lg bg-black/50 text-lg px-4 py-2"
+
+        class="group rounded-lg bg-black/50 text-lg px-4 py-2 cursor-pointer"
         :class="props?.class"
     >
-        <h2 class="font-eaves text-4xl border-b-2 border-red-800 flex justify-between cursor-pointer">
+        <DndHeading size="2" underline-color="red-800" underline class="flex w-full justify-between cursor-pointer">
             {{ title }}
-            <UButton
-                leading-icon="i-lucide-chevron-down"
-                size="xl"
-                class="group"
-                :ui="{
-                    trailingIcon: 'group-data-[state=open]:rotate-180 transition-transform duration-200'
-                }"
-            />
-        </h2>
+
+            <template #controls>
+
+            </template>
+        </DndHeading>
 
         <template #content>
             <slot/>
